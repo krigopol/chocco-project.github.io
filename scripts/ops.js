18e3cef49a0b84a2930a3364043991cbe116e9af
Dmitrii Kochetkov <dmitriikoch@gmail.com>
2020-07-15 00:06:04 +0300

    test task of 5th week

--- scripts/ops.js
+++ scripts/ops.js
@@ -1,51 +1,135 @@
-const sections = $('section');
+const sections = $('.section');
 const display = $('.main-content');
+const fixedMenu = $('.fixed-menu');
+const menuItems = fixedMenu.find('.fixed-menu__item');
+
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
 
 let inScroll = false;
 
 sections.first().addClass('visible');
 
-const perfTransition = (sectionEq) => {
-  if (inScroll === false) {
-    inScroll = true;
-    const position = sectionEq * -100;
+const countSectionPosition = (sectionEq) => {
+  const position = sectionEq * -100;
+  if (isNaN(position)) {
+    console.error('wrong value in countSectionPosition');
+    return 0;
+  }
 
-    display.css({ transform: `translateY(${position}%)` });
+  return position;
+};
 
-    sections
-      .eq(sectionEq)
-      .addClass('visible')
-      .siblings()
-      .removeClass('visible');
+const changeMenuThemeForSection = (sectionEq) => {
+  const currentSection = sections.eq(sectionEq);
+  const menuTheme = currentSection.attr('data-sidemenu-theme');
+  const activeClass = 'fixed-menu--shadowed';
 
-    setTimeout(() => {
-      inScroll = false;
-    }, 1300);
+  if (menuTheme === 'dark') {
+    fixedMenu.addClass(activeClass);
+  } else {
+    fixedMenu.removeClass(activeClass);
   }
 };
 
-const scrollViewport = (direct) => {
+const resetActiveClassForItem = (items, itemEq, activeClass) => {
+  items.eq(itemEq).addClass(activeClass).siblings().removeClass(activeClass);
+};
+
+const perfTransition = (sectionEq) => {
+  if (inScroll) return;
+
+  const trancitionOver = 300;
+  const mouseInertionOver = 200;
+
+  inScroll = true;
+
+  const position = countSectionPosition(sectionEq);
+
+  changeMenuThemeForSection(sectionEq);
+
+  display.css({ transform: `translateY(${position}%)` });
+
+  resetActiveClassForItem(sections, sectionEq, 'visible');
+
+  setTimeout(() => {
+    inScroll = false;
+
+    resetActiveClassForItem(menuItems, sectionEq, 'fixed-menu__item--active');
+  }, trancitionOver + mouseInertionOver);
+};
+
+const viewportScroller = () => {
   const visibleSection = sections.filter('.visible');
-  const nextSection = visibleSection.next();
+  const nextSection = visibleSection.next('.section');
   const prevSection = visibleSection.prev();
 
-  if (direct === 'next' && nextSection.length) {
-    perfTransition(nextSection.index());
-  }
-  if (direct === 'prev' && prevSection.length) {
-    perfTransition(prevSection.index());
-  }
+  return {
+    next() {
+      if (nextSection.length) {
+        perfTransition(nextSection.index());
+      }
+    },
+    prev() {
+      if (prevSection.length) {
+        perfTransition(prevSection.index());
+      }
+    },
+  };
 };
 
 $(window).on('wheel', (e) => {
   const deltaY = e.originalEvent.deltaY;
-  console.log('wheel');
+  const scroller = viewportScroller();
 
   if (deltaY > 0) {
-    scrollViewport('next');
+    scroller.next();
   }
 
   if (deltaY < 0) {
-    scrollViewport('prev');
+    scroller.prev();
   }
 });
+
+$(window).on('keydown', (e) => {
+  const tagName = e.target.tagName.toLowerCase();
+  const userTypingInInputs = tagName === 'textarea' || tagName === 'input';
+  const scroller = viewportScroller();
+
+  if (userTypingInInputs) return;
+
+  switch (e.keyCode) {
+    case 38:
+      scroller.prev();
+      break;
+    case 40:
+      scroller.next();
+      break;
+    case 32:
+      scroller.next();
+      break;
+  }
+});
+
+$('.wrapper').on('touchmove', (e) => e.preventDefault());
+
+$('[data-scroll-to]').click((e) => {
+  e.preventDefault();
+
+  const $this = $(e.currentTarget);
+  const target = $this.attr('data-scroll-to');
+  const reqSection = $(`[data-section-id=${target}]`);
+  perfTransition(reqSection.index());
+});
+
+if (isMobile) {
+  $('body').swipe({
+    swipe: function (event, direction) {
+      const scroller = viewportScroller();
+      let scrollDirection = '';
+      if (direction === 'up') scrollDirection = 'next';
+      if (direction === 'down') scrollDirection = 'prev';
+      scroller[scrollDirection]();
+    },
+  });
+}
